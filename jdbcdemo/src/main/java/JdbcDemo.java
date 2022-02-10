d3677e47f0af95124cd6dcf80cac91652cfa5429
tobiastilg <tobi.tilg@gmail.com>
2022-02-10 10:26:27 +0100

    neue Tabelle hinzugefügt

--- jdbcdemo/src/main/java/JdbcDemo.java
+++ jdbcdemo/src/main/java/JdbcDemo.java
@@ -5,21 +5,33 @@ public class JdbcDemo {
     public static void main(String[] args) {
         System.out.println("JDBC Demo!");
 
-        selectALlDemo();
+        /*selectAllStudentsDemo();
 
         insertStudentDemo("Martin Rieger", "martin@outlook.com");
-        selectALlDemo();
+        selectAllStudentsDemo();
 
-        updateStudentDemo(8, "Josef Reiter", "josef@outlook.com");
-        selectALlDemo();
+        updateStudentDemo(10, "Josef Reiter", "josef@outlook.com");
+        selectAllStudentsDemo();
 
-        deleteStudentDemo(8);
-        selectALlDemo();
+        deleteStudentDemo(10);
+        selectAllStudentsDemo();
 
-        findAllByNameLike("g");
+        findAllStudentsByNameLike("g");*/
+
+        selectAllAddressesDemo();
+        findAllAddressesByPostalCode("64");
+
+        insertAddressDemo("Ringstraße", "15b", "Wien", "1010");
+        selectAllAddressesDemo();
+
+        updateAddressDemo(6, "Teststraße", "1", "Zams", "6511");
+        selectAllAddressesDemo();
+
+        deleteAddressDemo(6);
+        selectAllAddressesDemo();
     }
 
-    private static void findAllByNameLike(String searchName) {
+    private static void findAllStudentsByNameLike(String searchName) {
         System.out.println("\nFind all by Name DEMO mit JDBC");
 
         String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
@@ -46,7 +58,7 @@ public class JdbcDemo {
         }
     }
 
-    public static void selectALlDemo() {
+    private static void selectAllStudentsDemo() {
         System.out.println("\nSelect DEMO mit JDBC");
 
         String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
@@ -72,7 +84,7 @@ public class JdbcDemo {
         }
     }
 
-    public static void insertStudentDemo(String name, String email) {
+    private static void insertStudentDemo(String name, String email) {
         System.out.println("\nInsert DEMO mit JDBC");
 
         String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
@@ -100,7 +112,7 @@ public class JdbcDemo {
         }
     }
 
-    public static void updateStudentDemo(int id, String newName, String newEmail) {
+    private static void updateStudentDemo(int id, String newName, String newEmail) {
         System.out.println("\nUpdate DEMO mit JDBC");
 
         String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
@@ -129,7 +141,7 @@ public class JdbcDemo {
         }
     }
 
-    public static void deleteStudentDemo(int studentId) {
+    private static void deleteStudentDemo(int studentId) {
         System.out.println("\nDelete DEMO mit JDBC");
 
         String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
@@ -146,7 +158,152 @@ public class JdbcDemo {
                 preparedStatement.setInt(1, studentId);
                 int rowAffected = preparedStatement.executeUpdate(); //liefert die Anzahl der betroffenen Datensätze
 
+                System.out.println(rowAffected + " Datensatz/Datensätze gelöscht");
+            } catch (SQLException ex) {
+                System.out.println("Fehler beim löschen eines Datensatzes: " + ex.getMessage());
+            }
+
+        } catch(SQLException e)  {
+            System.out.println("Fehler bei Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+    }
+
+    private static void selectAllAddressesDemo() {
+        System.out.println("\nSelect DEMO mit JDBC");
+
+        String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "123";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)) {
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM `address`");
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String street = rs.getString("street");
+                String houseNumber = rs.getString("house_number");
+                String location = rs.getString("location");
+                String plz = rs.getString("postal_code");
+                System.out.println("Adresse aus der DB: ID " + id + ", Straße " + street + ", Hausnummer " + houseNumber +
+                        ", Ort " + location + ", Postleitzahl " + plz);
+            }
+
+        } catch(SQLException e)  {
+            System.out.println("Fehler bei Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+    }
+
+    private static void findAllAddressesByPostalCode(String postalCode) {
+        System.out.println("\nFind all by Name DEMO mit JDBC");
+
+        String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "123";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)) {
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM `address` WHERE `address`.`postal_code` LIKE ?");
+            preparedStatement.setString(1,"%"+postalCode+"%");
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String street = rs.getString("street");
+                String houseNumber = rs.getString("house_number");
+                String location = rs.getString("location");
+                String plz = rs.getString("postal_code");
+                System.out.println("Adresse aus der DB: ID " + id + ", Straße " + street + ", Hausnummer " + houseNumber +
+                        ", Ort " + location + ", Postleitzahl " + plz);
+            }
+
+        } catch(SQLException e)  {
+            System.out.println("Fehler bei Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+    }
+
+    private static void insertAddressDemo(String street, String houseNumber, String location, String plz) {
+        System.out.println("\nInsert DEMO mit JDBC");
+
+        String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "123";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)) {
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "INSERT INTO `address` (`street`, `house_number`, `location`, `postal_code`) VALUES (?, ?, ?, ?)");
+
+            try {
+                preparedStatement.setString(1, street);
+                preparedStatement.setString(2, houseNumber);
+                preparedStatement.setString(3, location);
+                preparedStatement.setString(4, plz);
+                int rowAffected = preparedStatement.executeUpdate();
+
+                System.out.println(rowAffected + " Datensatz/Datensätze eingefügt");
+            } catch (SQLException ex) {
+                System.out.println("Fehler beim erstellen eines Datensatzes: " + ex.getMessage());
+            }
+
+        } catch(SQLException e)  {
+            System.out.println("Fehler bei Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+    }
+
+    private static void updateAddressDemo(int id, String newStreet, String newHouseNumber, String newLocation, String newPlz) {
+        System.out.println("\nInsert DEMO mit JDBC");
+
+        String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "123";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)) {
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "UPDATE `address` SET `street` = ?, `house_number` = ?, `location` = ?, `postal_code` = ? WHERE `address`.`id` = ?");
+
+            try {
+                preparedStatement.setString(1, newStreet);
+                preparedStatement.setString(2, newHouseNumber);
+                preparedStatement.setString(3, newLocation);
+                preparedStatement.setString(4, newPlz);
+                preparedStatement.setInt(5, id);
+                int rowAffected = preparedStatement.executeUpdate();
+
                 System.out.println(rowAffected + " Datensatz/Datensätze aktualisiert");
+            } catch (SQLException ex) {
+                System.out.println("Fehler beim erstellen eines Datensatzes: " + ex.getMessage());
+            }
+
+        } catch(SQLException e)  {
+            System.out.println("Fehler bei Aufbau der Verbindung zur DB: " + e.getMessage());
+        }
+    }
+
+    private static void deleteAddressDemo(int addressId) {
+        System.out.println("\nDelete DEMO mit JDBC");
+
+        String connectionUrl = "jdbc:mysql://10.77.0.110:3306/jdbcdemo";
+        String user = "root";
+        String pwd = "123";
+
+        try(Connection conn = DriverManager.getConnection(connectionUrl, user, pwd)) {
+            System.out.println("Verbindung zur DB hergestellt!");
+
+            PreparedStatement preparedStatement = conn.prepareStatement(
+                    "DELETE FROM `address` WHERE `address`.`id` = ?");
+
+            try {
+                preparedStatement.setInt(1, addressId);
+                int rowAffected = preparedStatement.executeUpdate();
+
+                System.out.println(rowAffected + " Datensatz/Datensätze gelöscht");
             } catch (SQLException ex) {
                 System.out.println("Fehler beim löschen eines Datensatzes: " + ex.getMessage());
             }
